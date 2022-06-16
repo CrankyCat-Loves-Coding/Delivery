58bd69e29eacb9a059118fcf45b3588c45560f37
CrankyCat-Loves-Coding <crankycatlovecoding@outlook.com>
2022-06-16 19:59:30 +0000

    remove get_cart_items for now to allow the website run without login

--- delivery/views.py
+++ delivery/views.py
@@ -11,17 +11,17 @@ class Main(View):
             customer = request.user.customer
             order, created = Order.objects.get_or_create(customer=customer, complete=False)
             items = order.orderitem_set.all()
-            cartItems = order.get_cart_items
+            # cartItems = order.get_cart_items
         else:
             items = []
             order = {'get_cart_total': 0, 'get_cart_item':0 }
-            cartItems = order['get_cart_items']
+            # cartItems = order['get_cart_items']
         
-        context = {
-            'cartItems': cartItems
-        }
+        # context = {
+        #     'cartItems': cartItems
+        # }
 
-        return render(request, 'index.html', context)
+        return render(request, 'index.html')
 
 
 class MenuItem(View):
@@ -37,17 +37,17 @@ class MenuItem(View):
             customer = request.user.customer
             order, created = Order.objects.get_or_create(customer=customer, complete=False)
             items = order.orderitem_set.all()
-            cartItems = order.get_cart_items
+            # cartItems = order.get_cart_items
         else:
             items = []
             order = {'get_cart_total': 0, 'get_cart_item':0 }
-            cartItems = order['get_cart_items']
+            # cartItems = order['get_cart_items']
 
         context = {
             'meals': meals,
             'desserts': desserts,
             'drinks': drinks,
-            'cartItems': cartItems
+            # 'cartItems': cartItems
         }
 
         return render(request, 'menu.html', context)
@@ -63,16 +63,16 @@ class Cart(View):
             customer = request.user.customer
             order, created = Order.objects.get_or_create(customer=customer, complete=False)
             items = order.orderitem_set.all()
-            cartItems = order.get_cart_items
+            # cartItems = order.get_cart_items
         else:
             items = []
             order = {'get_cart_total': 0, 'get_cart_item':0 }
-            cartItems = order['get_cart_items']
+            # cartItems = order['get_cart_items']
         
         context = {
             'items': items,
             'order': order,
-            'cartItems': cartItems
+            # 'cartItems': cartItems
         }
 
         return render(request, 'cart.html', context)
--- static/js/menu.js
+++ static/js/menu.js
@@ -40,6 +40,6 @@ function updateUserOrder(menuId, action){
 
     .then((data) =>{
         console.log('data:', data)
-        location.reload()
+        // location.reload()
     })
 }
